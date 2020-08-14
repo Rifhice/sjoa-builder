c85ef1bb786448460d0be052adf53c196821fc69
Kevin Giordani <giordani.kevin.02@gmail.com>
2020-08-14 16:51:40 +0200

    tests

--- src/__tests__/index.ts
+++ src/__tests__/index.ts
@@ -1,3 +1,620 @@
-describe("", () => {
-  test("", () => {});
+import * as utils from "..";
+const glob = require("glob");
+
+jest.mock("glob");
+
+describe("getFilesFromPath", () => {
+  afterAll(() => {
+    jest.resetModules();
+    jest.clearAllMocks();
+  });
+  test("Should return a array of files", async (done) => {
+    glob.mockImplementation((path, cb) => cb(undefined, ["lol"]));
+    const files = await utils.getFilesFromPath("*");
+    expect(files.every((file) => typeof file === "string")).toBe(true);
+    expect(files).toEqual(["lol"]);
+    done();
+  });
+  test("Should return an empty array on error", async (done) => {
+    glob.mockImplementation((path, cb) => cb("error"));
+    const files = await utils.getFilesFromPath("*");
+    expect(files).toEqual([]);
+    done();
+  });
+});
+
+describe("getFilesFromPaths", () => {
+  test("Should return an array of files", async (done) => {
+    //@ts-ignore
+    utils.getFilesFromPath = jest.fn().mockReturnValueOnce([]);
+    expect(await utils.getFilesFromPaths([])).toEqual([]);
+    //@ts-ignore
+    utils.getFilesFromPath = jest.fn().mockReturnValueOnce(["lol"]);
+    expect(await utils.getFilesFromPaths([])).toEqual([]);
+    //@ts-ignore
+    utils.getFilesFromPath = jest.fn().mockReturnValueOnce([]);
+    expect(await utils.getFilesFromPaths(["*"])).toEqual([]);
+    //@ts-ignore
+    utils.getFilesFromPath = jest.fn().mockReturnValueOnce(["lol"]);
+    expect(await utils.getFilesFromPaths(["*"])).toEqual(["lol"]);
+    done();
+  });
+  test("Should return a flat array of files", async (done) => {
+    //@ts-ignore
+    utils.getFilesFromPath = jest
+      .fn()
+      .mockReturnValueOnce(["lol"])
+      .mockReturnValueOnce(["slt"]);
+    const files = await utils.getFilesFromPaths(["*", "**/*"]);
+    expect(files).toEqual(["lol", "slt"]);
+    done();
+  });
+  test("Should return an empty array on error", async (done) => {
+    //@ts-ignore
+    utils.getFilesFromPath = jest.fn().mockImplementation(() => {
+      throw new Error("");
+    });
+    const files = await utils.getFilesFromPaths(["*", "**/*"]);
+    expect(files).toEqual([]);
+    done();
+  });
+
+  test("Should return an empty array with wrong parameters", async (done) => {
+    //@ts-ignore
+    expect(await utils.getFilesFromPaths()).toEqual([]);
+    //@ts-ignore
+    expect(await utils.getFilesFromPaths([3])).toEqual([]);
+    done();
+  });
+  test("Should return an empty array of files when no or invalid paths provided", async (done) => {
+    expect(await utils.getFilesFromPaths([])).toEqual([]);
+    //@ts-ignore
+    expect(await utils.getFilesFromPaths({})).toEqual([]);
+    //@ts-ignore
+    expect(await utils.getFilesFromPaths()).toEqual([]);
+    //@ts-ignore
+    expect(await utils.getFilesFromPaths(3)).toEqual([]);
+    //@ts-ignore
+    expect(await utils.getFilesFromPaths("le")).toEqual([]);
+    //@ts-ignore
+    expect(await utils.getFilesFromPaths(null)).toEqual([]);
+    //@ts-ignore
+    expect(await utils.getFilesFromPaths(false)).toEqual([]);
+    done();
+  });
+});
+
+describe("getExportedMembersFromFile", () => {
+  test("Should return false if no path is specified", () => {
+    //@ts-ignore
+    const result = utils.getExportedMembersFromFile();
+    expect(result).toEqual(false);
+  });
+  test("Should return false if file is not js", () => {
+    //@ts-ignore
+    global.require = () => ({ should: "not_work" });
+    const result = utils.getExportedMembersFromFile("test/lol.txt");
+    expect(result).toEqual(false);
+  });
+  test("Should return the exported members of a javascript file", () => {
+    //@ts-ignore
+    global.require = () => ({ should: "not_work" });
+    const result = utils.getExportedMembersFromFile("test/lol.js");
+    expect(result).toEqual({ should: "not_work" });
+  });
+  test("Should return false on error", () => {
+    //@ts-ignore
+    global.require = () => {
+      throw new Error("");
+    };
+    const result = utils.getExportedMembersFromFile("test/lol.js");
+    expect(result).toEqual(false);
+  });
+});
+
+describe("getFromFile", () => {
+  test("Should return false if either parameters is not specified", () => {
+    //@ts-ignore
+    expect(utils.getFromFile()).toEqual(false);
+    //@ts-ignore
+    expect(utils.getFromFile("test")).toEqual(false);
+    expect(utils.getFromFile(undefined, "test")).toEqual(false);
+  });
+  test("Should return the content of the key variableName from the first parameter", () => {
+    const result = utils.getFromFile({ salut: "lol" }, "salut");
+    expect(result).toEqual("lol");
+  });
+  test("Should return false if the variable doesn't exist", () => {
+    const result = utils.getFromFile({ salut: "lol" }, "mdr");
+    expect(result).toEqual(false);
+  });
+  test("Should return the false if the first parameters is not an object", () => {
+    expect(utils.getFromFile("kappa", "salut")).toEqual(false);
+    expect(utils.getFromFile(null, "salut")).toEqual(false);
+    expect(utils.getFromFile([], "salut")).toEqual(false);
+    expect(utils.getFromFile(3, "salut")).toEqual(false);
+    expect(utils.getFromFile(undefined, "salut")).toEqual(false);
+  });
+});
+
+describe("formatBaseStructure", () => {
+  test("Should not override values", () => {
+    const validBaseStructure = {
+      info: {
+        title: "title",
+        version: "version",
+      },
+      openapi: "3.0.0",
+      paths: {},
+    };
+    expect(utils.formatBaseStructure(validBaseStructure)).toEqual(
+      validBaseStructure
+    );
+  });
+  test("Should override 'openapi'", () => {
+    const validBaseStructure = {
+      info: {
+        title: "title",
+        version: "version",
+      },
+      openapi: "2.0.0",
+      paths: {},
+    };
+    expect(utils.formatBaseStructure(validBaseStructure)).toEqual({
+      ...validBaseStructure,
+      openapi: "3.0.0",
+    });
+  });
+  test("Should set default values", () => {
+    //@ts-ignore
+    expect(utils.formatBaseStructure({})).toEqual({
+      info: {
+        title: "app",
+        version: "1.0.0",
+      },
+      openapi: "3.0.0",
+      paths: {},
+    });
+  });
+  test("Should set default values", () => {
+    const validBaseStructure = {
+      info: {
+        title: "title",
+      },
+      openapi: "2.0.0",
+      paths: {},
+    };
+    //@ts-ignore
+    expect(utils.formatBaseStructure(validBaseStructure)).toEqual({
+      info: {
+        title: "title",
+        version: "1.0.0",
+      },
+      openapi: "3.0.0",
+      paths: {},
+    });
+  });
+});
+
+describe("validateRoutesOptions", () => {
+  test("Should throw if invalid parameters", () => {
+    expect(() =>
+      //@ts-ignore
+      utils.validateRoutesOptions(null)
+    ).toThrowError(Error);
+    expect(() =>
+      //@ts-ignore
+      utils.validateRoutesOptions(5)
+    ).toThrowError(Error);
+    expect(() =>
+      //@ts-ignore
+      utils.validateRoutesOptions("null")
+    ).toThrowError(Error);
+    expect(() =>
+      //@ts-ignore
+      utils.validateRoutesOptions([])
+    ).toThrowError(Error);
+    expect(() =>
+      //@ts-ignore
+      utils.validateRoutesOptions({})
+    ).toThrowError(Error);
+    expect(() =>
+      //@ts-ignore
+      utils.validateRoutesOptions(undefined)
+    ).toThrowError(Error);
+    expect(() =>
+      //@ts-ignore
+      utils.validateRoutesOptions({ globs: null })
+    ).toThrowError(Error);
+    expect(() =>
+      //@ts-ignore
+      utils.validateRoutesOptions({ globs: 5 })
+    ).toThrowError(Error);
+    expect(() =>
+      //@ts-ignore
+      utils.validateRoutesOptions({ globs: "null" })
+    ).toThrowError(Error);
+    expect(() =>
+      //@ts-ignore
+      utils.validateRoutesOptions({ globs: {} })
+    ).toThrowError(Error);
+    expect(() =>
+      //@ts-ignore
+      utils.validateRoutesOptions({ globs: [undefined] })
+    ).toThrowError(Error);
+    expect(() =>
+      //@ts-ignore
+      utils.validateRoutesOptions({ globs: [null] })
+    ).toThrowError(Error);
+    expect(() =>
+      //@ts-ignore
+      utils.validateRoutesOptions({ globs: [5] })
+    ).toThrowError(Error);
+    expect(() =>
+      //@ts-ignore
+      utils.validateRoutesOptions({ globs: [{}] })
+    ).toThrowError(Error);
+    expect(() =>
+      //@ts-ignore
+      utils.validateRoutesOptions({ converter: null })
+    ).toThrowError(Error);
+    expect(() =>
+      //@ts-ignore
+      utils.validateRoutesOptions({ converter: 5 })
+    ).toThrowError(Error);
+    expect(() =>
+      //@ts-ignore
+      utils.validateRoutesOptions({ converter: "null" })
+    ).toThrowError(Error);
+    expect(() =>
+      //@ts-ignore
+      utils.validateRoutesOptions({ converter: [] })
+    ).toThrowError(Error);
+    expect(() =>
+      //@ts-ignore
+      utils.validateRoutesOptions({ converter: {} })
+    ).toThrowError(Error);
+    expect(() =>
+      //@ts-ignore
+      utils.validateRoutesOptions({ converter: undefined })
+    ).toThrowError(Error);
+    expect(() =>
+      //@ts-ignore
+      utils.validateRoutesOptions({ variableName: null })
+    ).toThrowError(Error);
+    expect(() =>
+      //@ts-ignore
+      utils.validateRoutesOptions({ variableName: 5 })
+    ).toThrowError(Error);
+    expect(() =>
+      //@ts-ignore
+      utils.validateRoutesOptions({ variableName: [] })
+    ).toThrowError(Error);
+    expect(() =>
+      //@ts-ignore
+      utils.validateRoutesOptions({ variableName: {} })
+    ).toThrowError(Error);
+    //@ts-ignore
+    expect(() =>
+      //@ts-ignore
+      utils.validateRoutesOptions({ variableName: undefined })
+    ).toThrowError(Error);
+  });
+});
+describe("validateSchemasOptions", () => {
+  test("Should throw if invalid parameters", () => {
+    expect(() =>
+      //@ts-ignore
+      utils.validateSchemasOptions(null)
+    ).toThrowError(Error);
+    expect(() =>
+      //@ts-ignore
+      utils.validateSchemasOptions(5)
+    ).toThrowError(Error);
+    expect(() =>
+      //@ts-ignore
+      utils.validateSchemasOptions("null")
+    ).toThrowError(Error);
+    expect(() =>
+      //@ts-ignore
+      utils.validateSchemasOptions([])
+    ).toThrowError(Error);
+    expect(() =>
+      //@ts-ignore
+      utils.validateSchemasOptions({})
+    ).toThrowError(Error);
+    expect(() =>
+      //@ts-ignore
+      utils.validateSchemasOptions(undefined)
+    ).toThrowError(Error);
+    expect(() =>
+      //@ts-ignore
+      utils.validateSchemasOptions({ globs: null })
+    ).toThrowError(Error);
+    expect(() =>
+      //@ts-ignore
+      utils.validateSchemasOptions({ globs: 5 })
+    ).toThrowError(Error);
+    expect(() =>
+      //@ts-ignore
+      utils.validateSchemasOptions({ globs: "null" })
+    ).toThrowError(Error);
+    expect(() =>
+      //@ts-ignore
+      utils.validateSchemasOptions({ globs: {} })
+    ).toThrowError(Error);
+    expect(() =>
+      //@ts-ignore
+      utils.validateSchemasOptions({ globs: [undefined] })
+    ).toThrowError(Error);
+    expect(() =>
+      //@ts-ignore
+      utils.validateSchemasOptions({ globs: [null] })
+    ).toThrowError(Error);
+    expect(() =>
+      //@ts-ignore
+      utils.validateSchemasOptions({ globs: [5] })
+    ).toThrowError(Error);
+    expect(() =>
+      //@ts-ignore
+      utils.validateSchemasOptions({ globs: [{}] })
+    ).toThrowError(Error);
+    expect(() =>
+      //@ts-ignore
+      utils.validateSchemasOptions({ converter: null })
+    ).toThrowError(Error);
+    expect(() =>
+      //@ts-ignore
+      utils.validateSchemasOptions({ converter: 5 })
+    ).toThrowError(Error);
+    expect(() =>
+      //@ts-ignore
+      utils.validateSchemasOptions({ converter: "null" })
+    ).toThrowError(Error);
+    expect(() =>
+      //@ts-ignore
+      utils.validateSchemasOptions({ converter: [] })
+    ).toThrowError(Error);
+    expect(() =>
+      //@ts-ignore
+      utils.validateSchemasOptions({ converter: {} })
+    ).toThrowError(Error);
+    expect(() =>
+      //@ts-ignore
+      utils.validateSchemasOptions({ converter: undefined })
+    ).toThrowError(Error);
+    expect(() =>
+      //@ts-ignore
+      utils.validateSchemasOptions({ variableName: null })
+    ).toThrowError(Error);
+    expect(() =>
+      //@ts-ignore
+      utils.validateSchemasOptions({ variableName: 5 })
+    ).toThrowError(Error);
+    expect(() =>
+      //@ts-ignore
+      utils.validateSchemasOptions({ variableName: [] })
+    ).toThrowError(Error);
+    expect(() =>
+      //@ts-ignore
+      utils.validateSchemasOptions({ variableName: {} })
+    ).toThrowError(Error);
+    //@ts-ignore
+    expect(() =>
+      //@ts-ignore
+      utils.validateSchemasOptions({ variableName: undefined })
+    ).toThrowError(Error);
+  });
+});
+describe("extractPathsFromRouteFiles", () => {
+  test("Should return non converted route", async (done) => {
+    //@ts-ignore
+    utils.getFilesFromPaths = jest
+      .fn()
+      .mockResolvedValue(["my file", "my file 2"]);
+    //@ts-ignore
+    utils.getExportedMembersFromFile = jest.fn().mockReturnValue({
+      route: {
+        salut: { hey: "mdr" },
+      },
+    });
+    const paths = await utils.extractPathsFromRouteFiles({
+      globs: [],
+    });
+    expect(paths).toEqual([
+      ["salut", { hey: "mdr" }],
+      ["salut", { hey: "mdr" }],
+    ]);
+    done();
+  });
+  test("Should return converted route", async (done) => {
+    //@ts-ignore
+    utils.getFilesFromPaths = jest
+      .fn()
+      .mockResolvedValue(["my file", "my file 2"]);
+    //@ts-ignore
+    utils.getExportedMembersFromFile = jest.fn().mockReturnValue({
+      route: {
+        salut: { hey: "mdr" },
+      },
+    });
+    const paths = await utils.extractPathsFromRouteFiles({
+      globs: [],
+      //@ts-ignore
+      converter: (value) => ({ mdr: "lol" }),
+    });
+    expect(paths).toEqual([
+      ["mdr", "lol"],
+      ["mdr", "lol"],
+    ]);
+    done();
+  });
+});
+
+describe("extractPathsFromRouteFiles", () => {
+  test("Should return non converted route", async (done) => {
+    //@ts-ignore
+    utils.getFilesFromPaths = jest
+      .fn()
+      .mockResolvedValue(["my file", "my file 2"]);
+    //@ts-ignore
+    utils.getExportedMembersFromFile = jest.fn().mockReturnValue({
+      route: {
+        salut: { hey: "mdr" },
+      },
+    });
+    const paths = await utils.extractPathsFromRouteFiles({
+      globs: [],
+    });
+    expect(paths).toEqual([
+      ["salut", { hey: "mdr" }],
+      ["salut", { hey: "mdr" }],
+    ]);
+    done();
+  });
+  test("Should return converted route", async (done) => {
+    //@ts-ignore
+    utils.getFilesFromPaths = jest
+      .fn()
+      .mockResolvedValue(["my file", "my file 2"]);
+    //@ts-ignore
+    utils.getExportedMembersFromFile = jest.fn().mockReturnValue({
+      route: {
+        salut: { hey: "mdr" },
+      },
+    });
+    const paths = await utils.extractPathsFromRouteFiles({
+      globs: [],
+      //@ts-ignore
+      converter: (value) => ({ mdr: "lol" }),
+    });
+    expect(paths).toEqual([
+      ["mdr", "lol"],
+      ["mdr", "lol"],
+    ]);
+    done();
+  });
+});
+
+describe("build", () => {
+  test("Should throw error if either arguments is omitted or is not an object", () => {
+    //@ts-ignore
+    expect(() => utils.build({})).rejects.toBeInstanceOf(Error);
+    //@ts-ignore
+    expect(() => utils.build(3)).rejects.toBeInstanceOf(Error);
+    //@ts-ignore
+    expect(() => utils.build("lol")).rejects.toBeInstanceOf(Error);
+    //@ts-ignore
+    expect(() => utils.build(null)).rejects.toBeInstanceOf(Error);
+    //@ts-ignore
+    expect(() => utils.build(false)).rejects.toBeInstanceOf(Error);
+    //@ts-ignore
+    expect(() => utils.build([])).rejects.toBeInstanceOf(Error);
+  });
+  test("Should throw error if base structure is invalid", (done) => {
+    const options = {
+      routes: {
+        globs: [],
+      },
+      schemas: {
+        globs: [],
+      },
+    };
+    expect(() =>
+      utils.build({ ...options, baseStructure: null })
+    ).rejects.toBeInstanceOf(Error);
+    expect(() =>
+      //@ts-ignore
+      utils.build({ ...options, baseStructure: 5 })
+    ).rejects.toBeInstanceOf(Error);
+    expect(() =>
+      //@ts-ignore
+      utils.build({ ...options, baseStructure: "null" })
+    ).rejects.toBeInstanceOf(Error);
+    expect(() =>
+      //@ts-ignore
+      utils.build({ ...options, baseStructure: [] })
+    ).rejects.toBeInstanceOf(Error);
+    expect(() =>
+      utils.build({ ...options, baseStructure: undefined })
+    ).rejects.toBeInstanceOf(Error);
+    done();
+  });
+  test("Should call formatBaseStructure once", () => {
+    const spy = jest.spyOn(utils, "formatBaseStructure");
+    utils.build({
+      //@ts-ignore
+      baseStructure: {},
+      routes: {
+        globs: [],
+      },
+      schemas: {
+        globs: [],
+      },
+    });
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+  test("Should call validateRoutesOptions", () => {
+    const spy = jest.spyOn(utils, "validateRoutesOptions");
+    utils.build({
+      //@ts-ignore
+      baseStructure: {},
+      routes: {
+        globs: [],
+      },
+      schemas: {
+        globs: [],
+      },
+    });
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+  test("Should call validateSchemasOptions", () => {
+    const spy = jest.spyOn(utils, "validateSchemasOptions");
+    utils.build({
+      //@ts-ignore
+      baseStructure: {},
+      routes: {
+        globs: [],
+      },
+      schemas: {
+        globs: [],
+      },
+    });
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+  test("Should call extractPathsFromRouteFiles", () => {
+    const spy = jest.spyOn(utils, "extractPathsFromRouteFiles");
+    utils.build({
+      //@ts-ignore
+      baseStructure: {},
+      routes: {
+        globs: ["*/*"],
+      },
+      schemas: {
+        globs: [],
+      },
+    });
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+
+  test("Should add paths to doc", async (done) => {
+    //@ts-ignore
+    utils.extractPathsFromRouteFiles = jest
+      .fn()
+      .mockResolvedValueOnce([["lol", { great: "post" }]]);
+    const result = await utils.build({
+      //@ts-ignore
+      baseStructure: {},
+      routes: {
+        globs: ["*/*"],
+      },
+      schemas: {
+        globs: [],
+      },
+    });
+    expect(result.paths).toEqual({
+      lol: { great: "post" },
+    });
+    done();
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,9 @@ import {
 const glob = require("glob");
 const path = require("path");
 
+export const isAnObject = (value: any) =>
+  typeof value === "object" && value !== null && !Array.isArray(value);
+
 export const getFilesFromPath = (path: string): Promise<string[]> => {
   return new Promise((resolve) =>
     glob(path, async function (err, res: string[]) {
@@ -22,20 +25,24 @@ export const getFilesFromPaths = async (
   pathRegexes: string[]
 ): Promise<string[]> => {
   try {
-    const files = (
-      await Promise.all(
-        pathRegexes.map<Promise<string[]>>((path) => getFilesFromPath(path))
-      )
-    ).flat();
-    return files;
+    if (
+      !Array.isArray(pathRegexes) ||
+      !pathRegexes.every((path) => typeof path === "string")
+    )
+      return [];
+    const filesPromises = pathRegexes.map<Promise<string[]>>((path) =>
+      getFilesFromPath(path)
+    );
+    return (await Promise.all(filesPromises)).flat();
   } catch (error) {
     return [];
   }
 };
 
-export const getFileContent = (filePath: string) => {
+export const getExportedMembersFromFile = (filePath: string) => {
   try {
-    const requiredFile = require(path.join(process.cwd(), filePath));
+    if (!filePath || !filePath.endsWith(".js")) return false;
+    const requiredFile = global.require(path.join(process.cwd(), filePath));
     return requiredFile;
   } catch (error) {
     return false;
@@ -46,59 +53,111 @@ export function getFromFile<T>(
   fileContent: any,
   variableName: string
 ): false | T {
-  if (!fileContent || !variableName) return false;
-  if (fileContent[variableName]) return fileContent[variableName];
-  return false;
+  if (!fileContent || !variableName || !isAnObject(fileContent)) return false;
+  return fileContent[variableName] ?? false;
 }
 
-export const merge = async ({
-  baseStructure,
-  routes,
-  schemas,
-}: {
-  baseStructure: OpenAPIObject & { openapi: "3.0.0" };
-  routes: {
-    globs: string[];
-    variableName?: string;
-    converter?: (rawData: any) => PathsObject;
-  };
+export const formatBaseStructure = (
+  baseStructure: OpenAPIObject
+): OpenAPIObject => {
+  const copy = JSON.parse(JSON.stringify(baseStructure));
+  const defaultTitle = "app";
+  const defaultVersion = "1.0.0";
+  const defaultPaths = {};
+  copy.openapi = "3.0.0";
+  if (!copy.info)
+    copy.info = {
+      title: defaultTitle,
+      version: defaultVersion,
+    };
+  if (!copy.info.title) copy.info.title = defaultTitle;
+  if (!copy.info.version) copy.info.version = defaultVersion;
+  if (!copy.paths) copy.paths = defaultPaths;
+  return copy;
+};
+
+export type RouteOptions = {
+  globs: string[];
+  variableName?: string;
+  converter?: (rawData: any) => PathsObject;
+};
+
+export type SchemasOptions = {
+  globs: string[];
+  variableName?: string;
+  converter?: (rawData: any) => PathsObject;
+};
+
+export const validateRoutesOptions = (options: RouteOptions) => {
+  if (!isAnObject(options)) throw new Error("Routes should be an object");
+  if (
+    !options.globs ||
+    !Array.isArray(options.globs) ||
+    !options.globs.every((glob) => typeof glob === "string")
+  )
+    throw new Error("Routes glob should be an array of strings");
+  if (options.converter && typeof options.converter !== "function")
+    throw new Error("Routes converter should be a function");
+  if (options.variableName && typeof options.variableName !== "string")
+    throw new Error("Routes variable name should be a string");
+};
+
+export const validateSchemasOptions = (options: SchemasOptions) => {
+  if (!isAnObject(options)) throw new Error("Schemas should be an object");
+  if (
+    !options.globs ||
+    !Array.isArray(options.globs) ||
+    !options.globs.every((glob) => typeof glob === "string")
+  )
+    throw new Error("Schemas glob should be an array of strings");
+  if (options.converter && typeof options.converter !== "function")
+    throw new Error("Schemas converter should be a function");
+  if (options.variableName && typeof options.variableName !== "string")
+    throw new Error("Schemas variable name should be a string");
+};
+
+export const extractPathsFromRouteFiles = async (options: RouteOptions) => {
+  const { globs, variableName, converter } = options;
+  const routesPaths = await getFilesFromPaths(globs);
+  return routesPaths.reduce((acc, routePath) => {
+    const fileContent = getExportedMembersFromFile(routePath);
+
+    let routeDoc = getFromFile(fileContent, variableName || "route");
+    if (!routeDoc) return acc;
+
+    if (typeof converter === "function") routeDoc = converter(routeDoc);
+
+    acc.push(Object.entries(routeDoc)[0]);
+    return acc;
+  }, []);
+};
+
+export const build = async (options: {
+  baseStructure: OpenAPIObject;
+  routes: RouteOptions;
   schemas: {
     globs: string[];
     variableName?: string;
     converter?: (rawData: any) => SchemaObject | ReferenceObject;
   };
 }): Promise<OpenAPIObject> => {
+  if (!isAnObject(options)) throw new Error("Options should be an object");
+
+  const { routes, schemas } = options;
+
+  if (!isAnObject(options.baseStructure))
+    throw new Error("Base structure should be an object");
+  const baseStructure = formatBaseStructure(options.baseStructure);
+
+  validateRoutesOptions(routes);
+  validateSchemasOptions(schemas);
+
   const doc = new OpenApiBuilder(baseStructure);
 
   // Add paths to documentation
-  const routesPaths = await getFilesFromPaths(routes.globs);
-  for (const routePath of routesPaths) {
-    const fileContent = getFileContent(routePath);
-    let routeDoc = getFromFile(fileContent, routes.variableName || "route");
-    if (!routeDoc) continue;
-
-    if (typeof routes.converter === "function")
-      routeDoc = routes.converter(routeDoc);
-
-    const [path, route] = Object.entries(routeDoc)[0];
-
-    doc.addPath(path, route);
-  }
-
-  // Add schemas to documentation
-  const schemasPaths = await getFilesFromPaths(schemas.globs);
-  for (const schemaPath of schemasPaths) {
-    const fileContent = getFileContent(schemaPath);
-    let schemaDoc = getFromFile(fileContent, schemas.variableName || "schema");
-    if (!schemaDoc) continue;
-
-    if (typeof schemas.converter === "function")
-      schemaDoc = schemas.converter(schemaDoc);
-
-    const [path, schema] = Object.entries(schemaDoc)[0];
-
-    doc.addSchema(path, schema);
-  }
+  (await extractPathsFromRouteFiles(routes)).forEach(([path, route]) =>
+    doc.addPath(path, { ...route })
+  );
 
   return doc.getSpec();
 };
